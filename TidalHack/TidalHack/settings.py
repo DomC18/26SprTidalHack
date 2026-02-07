32caa1df3918894f4c75c974a150008573396aad
Khush <khush@example.com>
2026-02-07 14:58:49 -0600

    Fix ALLOWED_HOSTS to allow all hosts

--- TidalHack/TidalHack/settings.py
+++ TidalHack/TidalHack/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-700qjqqtku$7b8yphen_^d&q_^*ow9v$8!55ef8f$suu_7hokk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
